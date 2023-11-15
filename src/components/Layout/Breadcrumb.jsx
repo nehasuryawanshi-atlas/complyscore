c018f37e4ddc2a58a1d74a0883190d8be7352e3b
nehasuryawanshi-atlas <neha.suryawanshi@atlassystems.com>
2023-11-15 13:44:25 +0530

    test working

--- src/components/Layout/Breadcrumb.jsx
+++ src/components/Layout/Breadcrumb.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 export const Breadcrumb=()=>{
     return(
         <div>
-            <Typography color="primary" fontSize="0.75rem"><Link to='/'>Dashboards</Link>&nbsp;&#10187;</Typography>         
+            <Typography color="primary" fontSize="0.75rem"><Link to='/'>Dashboard</Link>&nbsp;&#10187;</Typography>         
         </div>
     )
 }
